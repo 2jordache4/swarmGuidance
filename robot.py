8118037867f2c3e18fe26dd3d60b75f74bca185d
2jordache4 <cousineau.jordan24@gmail.com>
2024-10-07 15:28:55 -0400

    Just some robot stuff from earlier hws

--- robot.py
+++ robot.py
@@ -3,9 +3,11 @@ Representation of a simple robot used in the assignments
 """
 
 import numpy as np
-from matplotlib import plot as plt
+from matplotlib import pyplot as plt
+
 
 class Robot:
+
     def __init__(self, vertices):
         """
         Save the input coordinates to the internal attribute vertices.
@@ -15,7 +17,13 @@ class Robot:
     def num_vertices(self):
         """ Number of vertices """
         return self.vertices.shape[1]
-    
+
+    def vertices_loop(self):
+        """
+        Returns self.vertices with the first vertex repeated at the end
+        """
+        return np.hstack((self.vertices, self.vertices[:, [0]]))
+
     def plot(self, style):
         """
         Plot the polygon using Matplotlib.
@@ -23,7 +31,7 @@ class Robot:
         if len(style) == 0:
             style = 'k'
 
-        directions = np.diff(self.vertices_loop)
+        directions = np.diff(self.vertices_loop())
         plt.quiver(self.vertices[0, :],
                    self.vertices[1, :],
                    directions[0, :],
@@ -33,22 +41,30 @@ class Robot:
                    scale_units='xy',
                    scale=1.)
 
-# def polygons_add_x_reflection(vertices):
-#     """
-#     Given a sequence of vertices, adds other vertices by reflection
-#     along the x axis
-#     """
-#     vertices = np.hstack([vertices, np.fliplr(np.diag([1, -1]).dot(vertices))])
-#     return vertices
 
 
-# def polygons_generate():
-#     """
-#     Generate the polygons to be used for the two-link manipulator
-#     """
-#     vertices1 = np.array([[0, 5], [-1.11, -0.511]])
-#     vertices1 = polygons_add_x_reflection(vertices1)
-#     vertices2 = np.array([[0, 3.97, 4.17, 5.38, 5.61, 4.5],
-#                           [-0.47, -0.5, -0.75, -0.97, -0.5, -0.313]])
-#     vertices2 = polygons_add_x_reflection(vertices2)
-#     return (Robot(vertices1), Robot(vertices2))
+def polygons_add_x_reflection(vertices):
+    """
+    Given a sequence of vertices, adds other vertices by reflection
+    along the x axis
+    """
+    vertices = np.hstack([vertices, np.fliplr(np.diag([1, -1]).dot(vertices))])
+    return vertices
+
+
+def polygons_generate():
+    """
+    Generate the polygons to be used for the two-link manipulator
+    """
+    vertices1 = np.array([[0, 5], [-1.11, -0.511]])
+    vertices1 = polygons_add_x_reflection(vertices1)
+    vertices2 = np.array([[0, 3.97, 4.17, 5.38, 5.61, 4.5],
+                          [-0.47, -0.5, -0.75, -0.97, -0.5, -0.313]])
+    vertices2 = polygons_add_x_reflection(vertices2)
+    return (Robot(vertices1), Robot(vertices2))
+
+
+# polygon1, polygon2 = polygons_generate()
+# rob = polygon1
+# rob.plot('k')
+# plt.show()
